620d04be68ebd68d09a8372fe450b48fe0bae275
Witold Adamus <witekadamus@gmail.com>
2025-09-16 15:21:19 +0800

    No dependency modal

--- chronology_presenter/src/components/publication/PublicationDetails.tsx
+++ chronology_presenter/src/components/publication/PublicationDetails.tsx
@@ -1,4 +1,3 @@
-import { Modal, Button } from 'react-daisyui';
 import type Publication from '../../data/dto/Publication';
 import type Person from '../../data/dto/Person';
 
@@ -15,39 +14,55 @@ const PublicationDetails = (props: PublicationDetailsProps) => {
   const appBasePath = '/history_of_philosophy/';
 
   return (
-    <Modal open={props.displayModal}>
-      <form method="dialog">
-        <Button
-          size="sm"
-          color="ghost"
-          shape="circle"
-          className="absolute right-2 top-2"
-          onClick={() => props.setDisplayModal(false)}
+    <div
+      onClick={() => props.setDisplayModal(false)}
+      className={
+        'fixed inset-0 bg-gray-900/50 backdrop-blur-xs backdrop-brightness-200 ' +
+        (props.displayModal ? '' : ' hidden')
+      }
+    >
+      <div className="flex items-center  justify-center h-full">
+        <div
+          style={{
+            background: 'rgba(8, 8, 11, 1)',
+          }}
+          className="drop-shadow-xl drop-shadow-cyan-500/50 rounded-xl w-[500px] border-r-20 border-pink-700"
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
         >
-          x
-        </Button>
-      </form>
+          <button
+            type="button"
+            onClick={() => props.setDisplayModal(false)}
+            className=" cursor-pointer text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 -right-5 -top-5 absolute"
+          >
+            X
+          </button>
 
-      <Modal.Header className="font-bold italic ">
-        &bdquo;{props.currentPublication.title}&rdquo;
-      </Modal.Header>
-      <Modal.Body className="pt-5">
-        {props.currentAuthor.name} - {props.currentPublication.publicationDate}{' '}
-        {props.currentPublication.isbn && ', ' + props.currentPublication.isbn}
-        {props.currentPublication.thumbnail && (
-          <img
-            src={
-              appBasePath +
-              '/assets/publication/' +
-              props.currentPublication.thumbnail
-            }
-            width={500}
-            height={700}
-            className="pt-5"
-          />
-        )}
-      </Modal.Body>
-    </Modal>
+          <div className="text-3xl text-white italic p-5">
+            &bdquo;{props.currentPublication.title}&rdquo;
+          </div>
+          <div className="p-5">
+            {props.currentAuthor.name} -{' '}
+            {props.currentPublication.publicationDate}
+            {props.currentPublication.isbn &&
+              ', ' + props.currentPublication.isbn}
+          </div>
+          {props.currentPublication.thumbnail && (
+            <img
+              src={
+                appBasePath +
+                '/assets/publication/' +
+                props.currentPublication.thumbnail
+              }
+              width={500}
+              height={700}
+              className="pt-5"
+            />
+          )}
+        </div>
+      </div>
+    </div>
   );
 };
 
